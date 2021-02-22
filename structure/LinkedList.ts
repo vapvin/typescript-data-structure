ba0b22bc649fe74e13e18cbd7bbff6a02d4bcdd5
vapvin <tpehdj12@daum.net>
2021-02-22 12:37:22 +0900

    Linked List Class Method

--- structure/LinkedList.ts
+++ structure/LinkedList.ts
@@ -32,4 +32,68 @@ class LinkedList<T> {
             currentItem = currentItem.next;
         }
     }
+
+    public isEmpty():boolean {
+        return this.head.next === this.tail;
+    }
+
+    public push(value:T): void {
+        this.addAfter(value, this.tail.prev);
+    }
+
+    public pop(): T {
+        let currentItem = this.tail.prev;
+        if(this.isEmpty()){
+            throw new Error(`The linked list is empty.`);
+        }
+        this.removeItem(currentItem);
+        return currentItem.value;
+    }
+
+    public remove(value: T): void {
+        let currentItem = this.search(value);
+        if(currentItem){
+            this.removeItem(currentItem);
+        } else {
+            throw new Error(`Caanot remove the value ${value}, it's not present in the linked list.`);
+        }
+    }
+
+    public shift(): T{
+        let item = this.head.next;
+        if(this.isEmpty()) {
+            throw new Error(`The linked list is empty.`);
+        }
+        this.removeItem(item);
+        return item.value;
+    }
+
+    public unshift(value:T): void{
+        this.addAfter(value, this.head);
+    }
+
+    private addAfter(value:T, itemAfter:LinkedListItem<T>): void{
+        let newItem = LinkedList.newItem(itemAfter, itemAfter.next, value);
+        itemAfter.next.prev = newItem;
+        itemAfter.next = newItem;
+        this.length++;
+    }
+
+    private removeItem(item:LinkedListItem<T>): void{
+        item.prev.next = item.next;
+        item.next.prev = item.prev;
+        this.length--;
+    }
+
+    private search(value:T): LinkedListItem<T>{
+        let currentItem = this.head.next;
+        while(currentItem !== this.tail) {
+            if (value === currentItem.value) {
+                return currentItem;
+            }
+            currentItem = currentItem.next;
+        }
+        return null;
+    }
+
 }
